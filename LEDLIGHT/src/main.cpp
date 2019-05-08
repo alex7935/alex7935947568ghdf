7a40c049eeb24be86964b7aacbf6dd097bf6658a
alex7935 <alexkrais07@gmail.com>
2019-05-08 18:09:21 +0300

    add new ailes

---
+++ LEDLIGHT/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+int led = 2;
+
+void setup() 
+{
+  pinMode(2,OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(led,HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+  delay(1000);
+}
